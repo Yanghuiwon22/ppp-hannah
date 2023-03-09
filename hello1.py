c815f4bc294892c0f57de4bb2680560062f1fb4d
Yanghuiwon22 <127187225+Yanghuiwon22@users.noreply.github.com>
2023-03-09 15:52:28 +0900

    과제01

--- hello1.py
+++ hello1.py
@@ -1,5 +1,4 @@
-weight = int(input("몸무게를 입력하세요(kg) => "))
-height = int(input("키를 입력하세요(cm) => ")) * 0.01
-BMI = weight / (height** 2)
+c=30
+fa=(c-32)*5/9
+print("{}℃ => {}℉".format(c, fa))
 
-print(BMI)
--- hello3.py
+++ hello3.py
@@ -1,8 +1,5 @@
-import math
+weight = 60
+height = 170*0.01
+BMI = weight / (height*height)
 
-
-weight = int(input("몸무게를 입력하세요(kg) => "))
-height = int(input("키를 입력하세요(cm) => ")) * 0.01
-BMI = weight / (math.pow(height, 2))
-
-print(("BMI는 {}".format(round(BMI, 2))))
+print(BMI)
