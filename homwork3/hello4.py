9ead5dc219bf9e281110f2380c68ebf7f7634475
Yanghuiwon22 <127187225+Yanghuiwon22@users.noreply.github.com>
2023-03-19 10:19:36 +0900

    Update hello4.py

--- homwork3/hello4.py
+++ homwork3/hello4.py
@@ -1,5 +1,12 @@
 import math
 
 ra = int(input("반지름의 길이는?"))
-area = math.pow(ra, 2)*3.14
+area = math.pow(ra, 2)*math.pi
 print  (area)
+length = 2*math.pi*ra
+
+print('원의 둘레는 {:04.1}입니다.'.format(length)}
+print('원의 넓이는 {:04.2}입니다.'.format(area)}
+
+
+
