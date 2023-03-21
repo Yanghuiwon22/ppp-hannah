58a9a601d911713dd52be874fb67017eb41056b8
Yanghuiwon22 <127187225+Yanghuiwon22@users.noreply.github.com>
2023-03-21 16:19:36 +0900

    Delete hello1.py

--- lec0321/hello1.py
+++
@@ -1,7 +0,0 @@
-mart ={"우유":2800, "계란":300, "빵":1200, "물":1700}
-cart=["물","물","계란","빵","빵","빵"]
-total_cost=0
-for item in cart:
-    if item=="물":
-        total_cost += 1700
-print(f"총 구매금액은 {total_cost}원입니다.")
