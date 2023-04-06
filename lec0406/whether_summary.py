e3ce14ae0d79a8f3a9c7469e9c77b8d07da2e178
Yanghuiwon22 <127187225+Yanghuiwon22@users.noreply.github.com>
2023-04-06 15:42:11 +0900

    Add files via upload

---
+++ lec0406/whether_summary.py
@@ -0,0 +1,82 @@
+def read_file(filename):
+    rainfalls = []
+    with open(filename) as f:
+        line_num = 0
+        for line in f:
+
+            print(line.split((",")))
+
+            line_num += 1
+            if line_num == 1:
+                continue
+            # 방법 1
+            tokens = line.strip().split(",")
+            rainfall = float(tokens[9])
+            rainfalls.append(rainfall)
+            print(rainfall)
+
+            # 방법 2
+            # tokens = line.split(",")
+            # rain = float(tokens[9])
+            # print(rain)
+        #
+        # for i,line in enumerate(f):
+        #     if 1 == 0:
+        #         continue
+        #     tokens = line.split(",")
+        #     rain = float(tokens[9])
+        #     print(rain)
+
+    return rainfalls
+
+def count_rain_days(rainfalls):
+    count = 0
+    for num in rainfalls:
+         if num >= 0.1:
+             count += 1
+    return count
+
+def longest_rain_days(rainfalls):
+    longest_days = 0
+    days = 0
+    for nums in rainfalls:
+        if nums > 0.0:
+            longest_days += 1
+        else:
+            if longest_days > days:
+                days = longest_days
+            else:
+                longest_days = 0
+
+    rainyday_count = 0
+    rain_event_days=[]
+    for rainfall in rainfalls:
+        if rainfall > 0:
+            rainyday_count += 1
+        if rainfall == 0 and rainyday_count > 0:
+            rain_event_days.append(rainyday_count)
+
+
+
+
+
+
+
+    return days
+def main():
+    filename = "weather(146)_2022-2022 (1).csv"
+    rainfall = read_file(filename)
+
+#     1) 총 강수량은?
+    print(f"총 강수량{sum(rainfall):.1f}mm입니다.")
+#     2) 강우 일수는 ?
+    print(f"총 강우일수는 {count_rain_days(rainfall):.1f}일입니다.")
+#     3)
+#     4) 최장연속강우일수는
+    print(f"최장연속강우일수는 {longest_rain_days(rainfall)}일입니다.")
+#     5)
+
+
+#
+if __name__=="__main__":
+    main()
