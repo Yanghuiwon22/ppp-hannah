2598c520806e45d6ba8f88fed5ea73f6484bf8aa
Yanghuiwon22 <127187225+Yanghuiwon22@users.noreply.github.com>
2023-05-11 13:18:09 +0900

    Add files via upload

--- homework15/high2low_taegon.py
+++ homework15/high2low_taegon.py
@@ -2,25 +2,35 @@
 def toggle_text(text:str):
     text_list = []
     for i in text:
-        if 65 <= ord(i) <=90:
-            char1 = ord(i) - ord("A")
-            char2 = chr(ord("a") + char1)
-            # print(char2)
-            # print(char1)
-            text_list.append(char2)
-        elif 97 <= ord(i) <= 112:
-            char3 = ord(i) - ord("a")
-            char4 = chr(ord("A") + char3)
-            # print(char4)
+        try:
+            if 65 <= ord(i) <=90:
+                char1 = ord(i) - ord("A")
+                char2 = chr(ord("a") + char1)
+                # print(char2)
+                # print(char1)
+                text_list.append(char2)
+            elif 97 <= ord(i) <= 112:
+                char3 = ord(i) - ord("a")
+                char4 = chr(ord("A") + char3)
+                # print(char4)
 
-            # print(char2)
-            text_list.append(char4)
-        else:
+                # print(char2)
+                text_list.append(char4)
+                # print(ord("a"))
+            # if 65 <= ord(i) <=90 or 97 <= ord(i) <= 112:
+            #     print(f"{text} => {''.join(toggle_text(text))}")
+
+
+
+        except:
             pass
-
-            # print(ord("a"))
     return text_list
 
+# def only_en(text):
+#     for i in text:
+#         if 65 <= ord(i) <=90 or 97 <= ord(i) <= 112:
+#
+#
 
 
 def main():
