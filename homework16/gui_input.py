1410c50e6d941c715f76318e44ac7d510702f7f2
Yanghuiwon22 <127187225+Yanghuiwon22@users.noreply.github.com>
2023-05-15 16:25:16 +0900

    Delete gui_input.py

--- homework16/gui_input.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import simpledialog
-window = tk.Tk()
-window.withdraw()
-
-def gui_input(text: str) -> str:
-    return simpledialog.askstring(title="Test", prompt=text)
